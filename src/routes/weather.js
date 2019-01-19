c5b790b698530e0c9a2a32a1ced97c0ea657e460
yuetongfeng <ryfprivate@outlook.com>
2019-01-19 17:02:03 +1100

    Added current weather route

---
+++ src/routes/weather.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('../utils/axios');
+
+const router = express.Router();
+
+router.get("/:cc/:city", (req, res) => {
+    const { cc, city } = req.params;
+    axios
+      .get(`/weather?q=${city},${cc}`)
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(err => console.log(err));
+});
+
+module.exports = router;
